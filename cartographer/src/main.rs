40ffba8a28185d26e42e1565a649831e1a222431
Taylor Newman <tanew7391@gmail.com>
2025-05-13 17:07:47 -0400

    Eureka!! Working A* pathfinding between start and end polygon. Created lifetime arguments for FaceNode successor function

--- cartographer/src/main.rs
+++ cartographer/src/main.rs
@@ -1,20 +1,20 @@
 #[macro_use]
 extern crate rocket;
 
-
-use pathfinding::prelude::astar;
+use std::env;
 use geo_postgis::FromPostgis;
+use pathfinding::prelude::astar;
 use postgis::ewkb;
 use proj::{Proj, Transform};
+use spade::PositionInTriangulation::OnFace;
 use tokio_postgres::{Client, NoTls};
-use spade::{PositionInTriangulation::{
-    OnEdge, OnFace, OnVertex
-}};
+use dotenv::dotenv;
 
-use geo::{
-    Contains, Coord, Within
+use geo::{Contains, Coord, Within};
+use spade::{
+    handles::{self, FaceHandle, FixedFaceHandle, InnerTag},
+    ConstrainedDelaunayTriangulation, Point2, Triangulation,
 };
-use spade::{handles::{self, FaceHandle, FixedFaceHandle, InnerTag}, ConstrainedDelaunayTriangulation, Point2, Triangulation};
 use std::{
     collections::{HashMap, HashSet},
     error::Error,
@@ -23,7 +23,7 @@ use std::{
 
 use log::{self, debug, info, warn};
 
-use geo_types::{GeometryCollection};
+use geo_types::GeometryCollection;
 
 use geojson::{Bbox, GeoJson, Geometry, PointType, Value};
 use rocket::{
@@ -114,7 +114,7 @@ fn extract_multipoint_geometry(geojson: GeoJson) -> Result<Vec<PointType>, Box<d
 }
 
 //Deprecated
-/* 
+/*
 fn multipoint_to_linestring(multipoints: Vec<PointType>) -> GeoJson {
     // Convert MultiPoint coordinates to LineString
     let linestring_coords = multipoints;
@@ -217,7 +217,7 @@ async fn index_old(data: Data<'_>) -> String {
 
 async fn get_connected_client() -> Result<tokio_postgres::Client, Box<dyn Error>> {
     let connection_attempt = tokio_postgres::connect(
-        "host=localhost port=5432 user=postgres password=postgres dbname=gisdb",
+        &format!("host=localhost port=5432 user=postgres password={} dbname=gisdb", env::var("db_password").unwrap_or("NO PASS SET".to_string())),
         NoTls,
     )
     .await;
@@ -229,7 +229,7 @@ async fn get_connected_client() -> Result<tokio_postgres::Client, Box<dyn Error>
             return Err(e.into());
         }
     };
-    
+
     // The connection object performs the actual communication with the database,
     // so spawn it off to run on its own.
     tokio::spawn(async move {
@@ -242,13 +242,7 @@ async fn get_connected_client() -> Result<tokio_postgres::Client, Box<dyn Error>
 }
 
 async fn get_focus_polygon_from_db(client: &Client) -> Result<geo_types::Polygon, Box<dyn Error>> {
-    
-    let rows = client
-        .query(
-            "SELECT polygon FROM focus_polygon",
-            &[],
-        )
-        .await;
+    let rows = client.query("SELECT polygon FROM focus_polygon", &[]).await;
 
     let rows = match rows {
         Ok(rows) => rows,
@@ -274,9 +268,9 @@ async fn get_focus_polygon_from_db(client: &Client) -> Result<geo_types::Polygon
     Ok(focus_polygon)
 }
 
-async fn retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(client: &Client
+async fn retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(
+    client: &Client,
 ) -> Result<(geo_types::Polygon, Vec<geo_types::Polygon<f64>>), Box<dyn Error>> {
-
     let rows = client
         .query("SELECT geom FROM holes WHERE path[1] = 0", &[])
         .await;
@@ -306,7 +300,7 @@ async fn retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(client: &
     let rows = client
         .query("SELECT geom FROM holes WHERE path[1] > 1", &[])
         .await;
-    
+
     let rows = match rows {
         Ok(rows) => rows,
         Err(e) => {
@@ -325,13 +319,8 @@ async fn retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(client: &
             warn!("Interior ring is None for row: {:?}", row);
         }
     }
-    Ok((
-        exterior_ring,
-        interior_rings,
-    ))
+    Ok((exterior_ring, interior_rings))
 }
-
-
 
 #[post("/api", data = "<data>")]
 async fn index(data: Data<'_>) -> String {
@@ -356,14 +345,14 @@ async fn index(data: Data<'_>) -> String {
         .await
         .expect("Failed to connect to database");
 
-    let (exterior_ring, interior_rings) = retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(&client)
+    let (exterior_ring, interior_rings) =
+        retrieve_exterior_and_interior_rings_of_focus_polygon_from_db(&client)
+            .await
+            .expect("Failed to retrieve exterior and interior rings");
+
+    let focus_polygon = get_focus_polygon_from_db(&client)
         .await
-        .expect("Failed to retrieve exterior and interior rings");
-
-    let focus_polygon = get_focus_polygon_from_db(&client).await
         .expect("Failed to retrieve focus polygon");
-
-
 
     let transform_to_wgs84 = Proj::new_known_crs(
         "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs",
@@ -412,6 +401,8 @@ async fn index(data: Data<'_>) -> String {
     println!("Last interior ring edge: {:?}", edges.last());
 
     //Debug mode
+
+    //TODO delete block
     let mut geometry_collection_vector: Vec<geo_types::Geometry> = vec![];
     for inner_face in cdt.inner_faces() {
         let vertices = inner_face.positions();
@@ -427,13 +418,13 @@ async fn index(data: Data<'_>) -> String {
 
     //let cdt: Result<ConstrainedDelaunayTriangulation<Point2<f64>>, spade::InsertionError> = ConstrainedDelaunayTriangulation::<_>::bulk_load_cdt(vertices, edges);
 
+    //TODO delete block
     let mut geometry_collection =
         geo_types::GeometryCollection::new_from(geometry_collection_vector);
     geometry_collection
         .transform(&transform_to_wgs84)
         .expect("Failed to reproject geometry collection");
 
-    let debug_string = Value::from(&geometry_collection).to_string();
 
     println!(
         "Number of Undirected Edges: {:}",
@@ -444,17 +435,16 @@ async fn index(data: Data<'_>) -> String {
     println!("Number of Faces: {:}", cdt.num_all_faces());
     println!("Number of Inner Faces: {:}", cdt.num_inner_faces());
 
-
     //Temp
-    let mut start = Coord{
+    let mut start = Coord {
         x: contained_multipoint_geometry[0][0],
         y: contained_multipoint_geometry[0][1],
     };
-    let mut end = Coord{
+    let mut end = Coord {
         x: contained_multipoint_geometry[1][0],
         y: contained_multipoint_geometry[1][1],
     };
-    
+
     start.transform(&transform_to_pseudo_mercator).unwrap();
     end.transform(&transform_to_pseudo_mercator).unwrap();
 
@@ -462,72 +452,38 @@ async fn index(data: Data<'_>) -> String {
     let end = Point2::new(end.x, end.y);
 
     //End Temp
-    
+
+    let polygon_path = compute_A_star_pathfinding_from_delaney(start, end, &cdt, &focus_polygon);
 
 
-    compute_A_star_pathfinding_from_delaney(
-        start,
-        end,
-        &cdt,
-        &focus_polygon
-    );
+    let test: Vec<geo_types::Geometry<f64>> = polygon_path
+        .iter()
+        .map(|face_node| geo_types::Geometry::from(face_to_polygon_TEST(face_node)))
+        .collect();
+    let mut test = geo_types::GeometryCollection::new_from(test);
+    test.transform(&transform_to_wgs84).expect("Failed to reproject geometry collection");
+    let debug_string = Value::from(&test).to_string();
 
     debug_string
 }
 
-
 //Start FaceNode
-#[derive(Clone, Debug)]
-struct FaceNode<'a>{
-    face: FaceHandle<'a, 
-        InnerTag, 
-        Point2<f64>, 
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct FaceNode<'a> {
+    face: FaceHandle<
+        'a,
+        InnerTag,
+        Point2<f64>,
         <ConstrainedDelaunayTriangulation<Point2<f64>> as Triangulation>::DirectedEdge,
         <ConstrainedDelaunayTriangulation<Point2<f64>> as Triangulation>::UndirectedEdge,
-        <ConstrainedDelaunayTriangulation<Point2<f64>> as Triangulation>::Face
+        <ConstrainedDelaunayTriangulation<Point2<f64>> as Triangulation>::Face,
     >,
-    focus_polygon: &'a geo_types::Polygon<f64>
 }
 
-impl PartialEq for FaceNode<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.face.eq(&other.face)
-    }
-}
-
-impl Eq for FaceNode<'_>{}
-
-impl std::hash::Hash for FaceNode<'_>{
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        //TODO: test if this is the correct way to hash
-        self.face.hash(state);
-    }
-}
-
-impl Ord for FaceNode<'_>{
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.face.cmp(&other.face)
-    }
-}
-
-impl PartialOrd for FaceNode<'_>{
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.face.cmp(&other.face))
-    }
-}
-
-impl FaceNode<'_>{
-
-    //TODO: see if this is discarding SigFigs which could be important for pathfinding
-    fn distance(&self, other: &FaceNode) -> u32 {
-        self.face.center().distance_2(other.face.center()).sqrt().round() as u32
-    }
-
-  
-    fn successors(&self) -> Vec<(FaceNode, u32)> {
-
+impl<'a> FaceNode<'a> {
+    fn successors(&self, focus_polygon: &geo_types::Polygon<f64>) -> Vec<(FaceNode<'a>, u32)> {
         let face_node = self.face;
-        let mut successor_faces: Vec<(FaceNode<'_>, u32)> = vec![];
+        let mut successor_faces: Vec<(FaceNode<'a>, u32)> = vec![];
         for edge in face_node.adjacent_edges() {
             let adjacent_face = edge.rev().face().as_inner();
             let adjacent_face = match adjacent_face {
@@ -537,31 +493,65 @@ impl FaceNode<'_>{
                     continue;
                 }
             };
-            let adjacent_face_node = FaceNode{
-                face: adjacent_face
+
+            let adjacent_center_point = adjacent_face.center();
+            let adjacent_center_point = Coord {
+                x: adjacent_center_point.x,
+                y: adjacent_center_point.y,
+            };
+
+            if !focus_polygon.contains(&adjacent_center_point) {
+                //We need only process the faces that are inside the focus polygon
+
+                //TODO: if profiling shows this is a bottleneck, we can use a hashset to store the face indices
+                //exterior_face_indices.insert(face_index);
+                continue;
+            }
+
+            let adjacent_face_node: FaceNode<'a> = FaceNode {
+                face: adjacent_face,
             };
             let distance_between_faces = self.distance(&adjacent_face_node);
-            successor_faces.push((   
-                adjacent_face_node,
-                distance_between_faces
-            ))
+            successor_faces.push((adjacent_face_node, distance_between_faces))
         }
 
-      successor_faces
+        successor_faces
     }
-  }
+}
 
+impl FaceNode<'_> {
+    //TODO: see if this is discarding SigFigs which could be important for pathfinding
+    fn distance(&self, other: &FaceNode) -> u32 {
+        self.face
+            .center()
+            .distance_2(other.face.center())
+            .sqrt()
+            .round() as u32
+    }
+}
+
+fn face_to_polygon_TEST(face_node: &FaceNode) -> geo_types::Polygon<f64> {
+    let face = face_node.face;
+    let vertices = face.positions();
+    let linestring = geo_types::LineString::from(
+        vertices
+            .into_iter()
+            .map(|c| Coord { x: c.x, y: c.y })
+            .collect::<Vec<Coord<f64>>>(),
+    );
+    geo_types::Polygon::new(linestring, vec![])
+}
 //End FaceNode
 
-fn compute_A_star_pathfinding_from_delaney(
+fn compute_A_star_pathfinding_from_delaney<'a>(
     start: Point2<f64>,
     end: Point2<f64>,
-    cdt: &ConstrainedDelaunayTriangulation<Point2<f64>>,
+    cdt: &'a ConstrainedDelaunayTriangulation<Point2<f64>>,
     focus_polygon: &geo_types::Polygon<f64>,
-) -> Vec<Point2<f64>> { //This should return a vector of faces from cdt
+) -> Vec<FaceNode<'a>> {
+    //This should return a vector of faces from cdt
     // Implement A* algorithm here using the CDT
     // This is a placeholder implementation
-    
 
     let mut exterior_face_indices: HashSet<usize> = HashSet::new();
     let mut processed_face_indices: HashMap<usize, Point2<f64>> = HashMap::new();
@@ -570,67 +560,62 @@ fn compute_A_star_pathfinding_from_delaney(
     //let start_transformed = start.transform(&transformer).unwrap();
     println!("Start transformed: {:?}", start);
     let position_in_triangulation = cdt.locate(start);
+
     //Todo: test if a start or end point is on a vertex or an edge
-    let start_face_handle: Option<FixedFaceHandle<InnerTag>> = None; 
+    let mut start_face_handle: Option<FixedFaceHandle<InnerTag>> = None;
     if let OnFace(face_handle) = position_in_triangulation {
         println!("On face: {:?}", face_handle.index());
-        let start_face_handle = Some(face_handle);
+        start_face_handle = Some(face_handle);
     } else {
         println!("Not on face, {:?}", position_in_triangulation);
         panic!("Start point is not on a face, no way to handle this yet");
     }
 
+    //Todo compress this into a single function
+    let position_in_triangulation = cdt.locate(end);
+    let mut end_face_handle: Option<FixedFaceHandle<InnerTag>> = None;
+    if let OnFace(face_handle) = position_in_triangulation {
+        println!("On face: {:?}", face_handle.index());
+        end_face_handle = Some(face_handle);
+    } else {
+        println!("Not on face, {:?}", position_in_triangulation);
+        panic!("Start point is not on a face, no way to handle this yet");
+    }
 
     let start_face_center_vertex = cdt.face(start_face_handle.unwrap());
+    let end_face_center_vertex = cdt.face(end_face_handle.unwrap());
 
+    let start_face_node = FaceNode {
+        face: start_face_center_vertex,
+    };
 
-    let a_star_results = astar(start_face_center_vertex.center(), successors, heuristic, success)
+    let end_face_node = FaceNode {
+        face: end_face_center_vertex,
+    };
 
-    //Uncomment when using for A star
-    // First, loop through the faces of the CDT and test for intersection with the focus polygon
-    /* for face in cdt.inner_faces() {
-        let center_point = face.center();
-        let center_point = Coord {
-            x: center_point.x,
-            y: center_point.y,
-        };
+    let heuristic_fn = |face_node: &FaceNode| face_node.distance(&end_face_node);
 
-        let face_index = face.index();
+    let sucessors_fn = |face_node: &FaceNode<'a>| -> Vec<(FaceNode<'a>, u32)> {
+        face_node.successors(focus_polygon)
+    };
 
-        if exterior_face_indices.contains(&face_index) {
-            // Skip if the face is already in the exterior face indices
-            continue;
+    //TODO: this eq might not be correct if it refers to the actual handle and not the face.
+    let success_fn = |face_node: &FaceNode| face_node.face == end_face_node.face;
+
+    let a_star_results = astar(&start_face_node, sucessors_fn, heuristic_fn, success_fn);
+    let a_star_results = match a_star_results {
+        Some((path, _cost)) => path,
+        None => {
+            println!("No path found");
+            return vec![];
         }
-        
-        if !focus_polygon.contains(&center_point) {
-            //We need only process the faces that are inside the focus polygon
-            exterior_face_indices.insert(face_index);
-            continue;
-        }
+    };
 
-        // Check if the face includes the start or end point
-        let polygon_for_face = cdt_face_to_polygon(&face.positions());
-
-        if (Coord{x: start.x, y: start.y}).is_within(&polygon_for_face){
-            //Handle start point
-        }
-
-        if (Coord{x: end.x, y: end.y}).is_within(&polygon_for_face){
-            //Handle end point
-        }
-
-
-        processed_face_indices.insert(face_index, face.center());
-    } */
-    
-    let mut path = vec![start, end];
-    path
+    a_star_results
 }
 
 //TODO: test
-fn cdt_face_to_polygon(
-    face_positions: &[Point2<f64>; 3],
-) -> geo_types::Polygon<f64> {
+fn cdt_face_to_polygon(face_positions: &[Point2<f64>; 3]) -> geo_types::Polygon<f64> {
     let linestring = geo_types::LineString::from(
         face_positions
             .into_iter()
@@ -639,9 +624,10 @@ fn cdt_face_to_polygon(
     );
     geo_types::Polygon::new(linestring, vec![])
 }
- 
+
 #[launch]
 fn rocket() -> _ {
+    dotenv().ok();
     rocket::build().mount("/", routes![index])
 }
 
